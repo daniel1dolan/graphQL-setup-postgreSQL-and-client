2757ea7da3d4448c7118ba14e1ad31c2f3c248f0
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-05-21 13:14:33 -0500

    express with graphql

--- full_setup/server/src/index.js
+++ full_setup/server/src/index.js
@@ -1,18 +1,37 @@
-import express from 'express'
-import {ApolloServer} from 'apollo-server-express'
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
 
-const app = express()
+const app = express();
 
-const schema = ...
-const resolvers = ...
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+//The Resolver is agnostic to the data source.
+//For instance, data is hardcoded to resolve the 'me' field.
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: "Robin Wieruch",
+      };
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
-})
+  typeDefs: schema,
+  resolvers,
+});
 
-server.applyMiddleWare({app, path: 'graphql'})
+server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen({port: 8000}, ()=>{
-    console.log('Apollo server listening on http://localhost:8000')
-})
+app.listen({ port: 8000 }, () => {
+  console.log("Apollo server listening on http://localhost:8000/graphql");
+});
