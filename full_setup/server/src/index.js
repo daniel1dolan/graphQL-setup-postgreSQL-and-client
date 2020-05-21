4f3b64cdb4442e6f632d93702c7ea69fa4e28f42
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-05-21 13:44:39 -0500

    multiple GraphQL queries and arguments

--- full_setup/server/src/index.js
+++ full_setup/server/src/index.js
@@ -1,25 +1,60 @@
 const express = require("express");
+const cors = require("cors");
 const { ApolloServer, gql } = require("apollo-server-express");
 
 const app = express();
 
+app.use(cors());
+
 const schema = gql`
   type Query {
     me: User
+    users: [User!]
+    user(id: ID!): User
+    company: Company
   }
 
   type User {
+    id: ID!
     username: String!
   }
+
+  type Company {
+    name: String!
+    yearFounded: Int!
+  }
 `;
+
+let users = {
+  1: {
+    id: "1",
+    username: "Rovin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+const me = users[1];
 
 //The Resolver is agnostic to the data source.
 //For instance, data is hardcoded to resolve the 'me' field.
 const resolvers = {
   Query: {
     me: () => {
+      return me;
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    users: () => {
+      return Object.values(users);
+    },
+    company: () => {
       return {
-        username: "Robin Wieruch",
+        name: "Rufus Inc.",
+        yearFounded: 2014,
       };
     },
   },
